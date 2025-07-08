46725db48092250f5e2da8e97e160964e124fa95
adnanmadi417 <uso53dr6i8s4yt0idkq7f6ivj04en8@bots.bitbucket.org>
2025-07-08 23:14:21 +0800

    updated colors into mian app

--- app.vue
+++ app.vue
@@ -3,3 +3,27 @@
     <NuxtPage/>
   </div>
 </template>
+
+
+<style>
+
+:root {
+  --primary-bg: #FDFCF9;
+  --secondary-bg: #F5F5F5;
+  --card-bg: #F1F1F1;
+  --border-color: #E0C281;
+  --accent-color: #FFC107;
+  --profit-color: #2E7D32;
+  --danger-color: #D32F2F;
+  --button-bg: #F5B301;
+  --hover-button-bg: #FFE599;
+  --hover-text-color: #2C2C2C;
+  --input-field-bg: #EBEBEB;
+  --cancel-button-bg: #DDDDDD;
+  --primary-text-color: #2C2C2C;
+  --secondary-text-color: #666666;
+
+}
+
+
+</style>
