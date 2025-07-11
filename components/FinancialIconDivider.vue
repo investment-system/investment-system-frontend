a20262a6dd7a86b8bfc551b75b6a7f157e963ce9
adnanmadi417 <uso53dr6i8s4yt0idkq7f6ivj04en8@bots.bitbucket.org>
2025-07-11 20:58:57 +0800

    created Financial Icon Divider

---
+++ components/FinancialIconDivider.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="icon-divider">
+    <span class="line"></span>
+    <UIcon name="mdi-bar-chart" class="icon" />
+    <span class="line"></span>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.icon-divider {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px 0;
+
+  .line {
+    flex: 1;
+    height: 2px;
+    background-color: var(--accent-color);
+  }
+
+  .icon {
+    margin: 0 12px;
+    color: var(--accent-color);
+    font-size: var(--heading-4);
+  }
+
+  @media (min-width: 768px) {
+    margin: 10px 0;
+
+    .icon {
+      font-size: var(--heading-3);
+    }
+  }
+
+  @media (min-width: 1024px) {
+    margin: 10px 0;
+
+    .icon {
+      font-size: var(--heading-2);
+    }
+  }
+}
+</style>
