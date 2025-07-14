aa4a148dfc2d313d5fe4b1de4e3524dcfb030f20
adnanmadi417 <uso53dr6i8s4yt0idkq7f6ivj04en8@bots.bitbucket.org>
2025-07-14 13:10:13 +0800

    debugging images path issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
 
   modules: ['@nuxt/ui'],
 
+
+  app: {
+    baseURL: process.env.NODE_ENV === '' ? '' : '/investment-system-frontend/',
+  },
+
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true }
 })
