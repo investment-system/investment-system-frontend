0176db71aed39fe60ad5ac8b19bebbafd6af0119
adnanmadi417 <uso53dr6i8s4yt0idkq7f6ivj04en8@bots.bitbucket.org>
2025-07-17 20:13:28 +0800

    created Email Confirmation component and  account Secure Your Account  page to handle verification process

---
+++ pages/member/secure-your-account.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import EmailConfirmation from "~/components/member/EmailConfirmation.vue";
+</script>
+
+<template>
+  <EmailConfirmation/>
+</template>
+
+<style scoped>
+
+</style>
