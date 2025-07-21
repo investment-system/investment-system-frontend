a7002d474e4eeb02c09241e58230ecb99d6532c5
adnanmadi417 <uso53dr6i8s4yt0idkq7f6ivj04en8@bots.bitbucket.org>
2025-07-21 11:20:01 +0800

    updated header label

--- components/member/Header.vue
+++ components/member/Header.vue
@@ -8,7 +8,7 @@ const router = useRouter()
 const links = [
   { id: 1, link: '/member/dashboard', label: 'Home', icon: 'i-lucide-home' },
   { id: 3, link: '/member/investments', label: 'Investments', icon: 'mdi-bar-chart' },
-  { id: 3, link: '/member/profile', label: 'Setting', icon: 'i-lucide-user' },
+  { id: 3, link: '/member/profile', label: 'Profile', icon: 'i-lucide-user' },
   { id: 4, link: '/', label: 'Logout', icon: 'i-lucide-log-out' }
 ]
 
