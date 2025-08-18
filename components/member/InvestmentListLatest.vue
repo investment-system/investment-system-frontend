7b1898d72dd5b6d081df981726cdc9f46e3d82f1
adnanmadi417 <uso53dr6i8s4yt0idkq7f6ivj04en8@bots.bitbucket.org>
2025-08-18 13:54:44 +0800

    1. push investment latest list into member side

--- components/member/InvestmentListLatest.vue
+++ components/member/InvestmentListLatest.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed, onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useApi } from '~/composables/useApi'
 
 interface Transaction {
